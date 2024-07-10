a55370e638140c1c47216c56b6901b378a6be2fc
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-07-10 09:05:28 -0500

    updated the grid layout for the projects section in services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Section from "./Section";
 import Heading from "./Heading";
 
-import { nexaBankImg, restaurantImg, airTourImg, futureWaveImg, creativeDesignImg, architectImg } from '../assets';
+import { photographerImg, boltiImg, madisonImg, nexaBankImg, restaurantImg, airTourImg, futureWaveImg, creativeDesignImg, architectImg } from '../assets';
 
 const ProjectCard = ({ image, title, description, link }) => (
   <a
@@ -23,12 +23,15 @@ const ProjectCard = ({ image, title, description, link }) => (
 
 const Services = () => {
   const projects = [
-    { image: restaurantImg, title: "Fine Dining Website", description: "Custom-built website with online menu and reservation system.", link: "https://trois-champs-elysees.netlify.app/" },
-    { image: nexaBankImg, title: "NexaBank Platform", description: "Modern user-friendly online banking interface.", link: "https://luke-nexabank.netlify.app/" },
-    { image: futureWaveImg, title: "FutureWave Capital", description: "Innovative product showcase for a cutting-edge tech capital company.", link: "https://future-wave.netlify.app/" },
-    { image: creativeDesignImg, title: "Creative Design Agency", description: "Visually stunning portfolio for a graphic design agency.", link: "https://creative-design-luke.netlify.app/" },
-    { image: airTourImg, title: "AirTour Booking System", description: "Comprehensive travel booking platform for air balloon tours.", link: "https://luke-airtour.netlify.app/" },
-    { image: architectImg, title: "Architect Showcase", description: "Elegant portfolio website for a renowned architecture firm.", link: "https://luke-architect.netlify.app/" },
+    { image: restaurantImg, title: "Fine Dining Website", description: "Custom-built website with online menu and reservation system", link: "https://trois-champs-elysees.netlify.app/" },
+    { image: nexaBankImg, title: "NexaBank Platform", description: "Modern user-friendly online banking interface", link: "https://luke-nexabank.netlify.app/" },
+    { image: futureWaveImg, title: "FutureWave Capital", description: "Innovative product showcase for a cutting-edge tech capital company", link: "https://future-wave.netlify.app/" },
+    { image: creativeDesignImg, title: "Creative Design Agency", description: "Visually stunning portfolio for a graphic design agency", link: "https://creative-design-luke.netlify.app/" },
+    { image: airTourImg, title: "AirTour Booking System", description: "Comprehensive travel booking platform for air balloon tours", link: "https://luke-airtour.netlify.app/" },
+    { image: architectImg, title: "Architect Showcase", description: "Elegant portfolio website for a renowned architecture firm", link: "https://luke-architect.netlify.app/" },
+    { image: photographerImg, title: "Photographer Website", description: "Visually striking photographer portfolio website", link: "https://luke-photographer.netlify.app/" },
+    { image: boltiImg, title: "E-Scooter Rental Website", description: "Vibrant and dynamic website advertising E-Scooters", link: "https://luke-bolti.netlify.app/" },
+    { image: madisonImg, title: "Public Library Website", description: "Aesthetically clean and simple website built for a public library", link: "https://madison-public-lbrary.netlify.app/" },
   ];
 
   return (
@@ -41,7 +44,7 @@ const Services = () => {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
           {projects.map((project, index) => (
-            <div key={index} className={`${index === 3 || index === 4 ? 'md:col-span-2 lg:col-span-1' : ''
+            <div key={index} className={`${index === 2.5 || index === 4 ? 'md:col-span-2 lg:col-span-1' : ''
               }`}>
               <ProjectCard {...project} />
             </div>
